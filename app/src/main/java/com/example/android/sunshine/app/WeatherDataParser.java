b8d810ad8cbff71e355ba3be43316a24f7c3fd96
Jose Manuel <jmsanchez07@gmail.com>
2016-07-03 13:06:58 +0200

    Cambios hasta el tema 4 del curso

---
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,16 @@
+package com.example.android.sunshine.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataParser {
+
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex) throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+}
